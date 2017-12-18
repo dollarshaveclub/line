832c90f3aecabc7b0dc730810ff7718620007ae0
Hank Jacobs <hank.jacobs@dollarshaveclub.com>
2017-12-18 13:07:56 -0800

    Be explicit about what the color constants are

--- color.go
+++ color.go
@@ -34,15 +34,16 @@ var (
 const escape = "\x1b"
 const reset = "0"
 
+// ansi escape sequence color constants
 const (
-	black = iota + 30
-	red
-	green
-	yellow
-	blue
-	magenta
-	cyan
-	white
+	black   = 30
+	red     = 31
+	green   = 32
+	yellow  = 33
+	blue    = 34
+	magenta = 35
+	cyan    = 36
+	white   = 37
 )
 
 // Color formats with a given color
